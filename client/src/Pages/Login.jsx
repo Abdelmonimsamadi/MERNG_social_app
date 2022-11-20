7a3af904ad084e172f22dc96f27b33a27b3bd245
abdul <monimesamadi@gmail.com>
2022-11-20 18:22:25 +0100

    react hook form added to login page with yup

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -5,17 +5,36 @@ import { Button, Form } from "semantic-ui-react";
 import { AuthContext } from "../context/auth";
 import { loginMutation } from "../utils/grahql";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    email: yup.string().email().required(),
+    password: yup.string().min(4).required(),
+  })
+  .required();
+
 const Login = () => {
+  const {
+    register: registerRHF,
+    handleSubmit: handleSubmitRHF,
+    formState: { errors: errorsRHF },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   const authContext = useContext(AuthContext);
   const navigate = useNavigate();
-  const [values, setValues] = useState({
-    email: "",
-    password: "",
-  });
+
   const [errors, setErrors] = useState({});
 
   const [login, { loading }] = useMutation(loginMutation, {
-    variables: values,
     update(_, { data: { loginUser } }) {
       authContext.loginOrRegister(loginUser);
       navigate("/");
@@ -25,19 +44,16 @@ const Login = () => {
     },
   });
 
-  const handleChange = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    login();
+  const handleSubmit = async (data) => {
+    await login({
+      variables: data,
+    });
   };
 
   return (
     <div className="form-container">
       <Form
-        onSubmit={handleSubmit}
+        onSubmit={handleSubmitRHF(handleSubmit)}
         style={{
           width: "400px",
           marginLeft: "auto",
@@ -47,21 +63,29 @@ const Login = () => {
       >
         <Form.Field error={errors.email ? true : false}>
           <label>Email</label>
-          <input placeholder="Email" name="email" onChange={handleChange} />
+          <input placeholder="Email" {...registerRHF("email")} />
         </Form.Field>
         <Form.Field error={errors.password ? true : false}>
           <label>Password</label>
           <input
             type="password"
             placeholder="Password"
-            name="password"
-            onChange={handleChange}
+            {...registerRHF("password")}
           />
         </Form.Field>
         <Button type="submit" loading={loading} disabled={loading}>
           Login
         </Button>
       </Form>
+      {Object.keys(errorsRHF).length > 0 && (
+        <div className="ui error message">
+          <ul>
+            {Object.values(errorsRHF).map((input, i) => (
+              <li key={i}>{input.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       {Object.keys(errors).length > 0 && (
         <div className="ui error message">
           <ul>
