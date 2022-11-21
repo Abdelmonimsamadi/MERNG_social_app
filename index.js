09412b23296cfff3d03c1dc5f6b735150c9c4f1d
abdul <monimesamadi@gmail.com>
2022-11-21 12:34:40 +0100

    change mongodb connection with perfect log

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import mongoose from 'mongoose';
+import connectDB from './config/db.js';
 
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
@@ -17,14 +17,12 @@ const PORT = parseInt(process.env.PORT) || 3001
 
 const start = async () => {
     try {
-        const mongoosePromise = mongoose.connect(process.env.MONGODB_IRI)
         const apolloServerPromise = startStandaloneServer(server, {
             listen: { port: PORT },
             context: ({ req }) => ({ req })
         })
         // const [_, { url }] = await Promise.all([mongoosePromise, apolloServerPromise])
-        await mongoosePromise
-        console.log("db connected ...");
+        await connectDB()
         const { url } = await apolloServerPromise
         console.log(`🚀  Server ready at: ${url}`);
     } catch (error) {
